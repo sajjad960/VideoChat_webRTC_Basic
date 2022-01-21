577a0de0bdf36d1aec18b032db1bc125d26bbeff
sajjad960 <sajjadhossainsb01@gmail.com>
2022-01-21 09:38:52 +0600

    created join,full,created dinametic

--- public/chat.js
+++ public/chat.js
@@ -5,16 +5,51 @@ let joinButton = document.getElementById("join");
 let userVideo = document.getElementById("user-video");
 let peerVideo = document.getElementById("peer-video");
 let roomInput = document.getElementById("roomName");
+let roomName = roomInput.value;
 
+let creator = false
 
 joinButton.addEventListener('click', function(){
   if(roomInput.value === '') {
     alert('Please enter a room name')
   } else {
-    socket.emit("join", roomInput.value)
-    async function getMedia(constraints) {
-      let stream = null;
+    socket.emit("join", roomName)
     
+  }
+})
+
+socket.on('created', function() {
+  let creator = true
+
+  async function getMedia(constraints) {
+  let stream = null;
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        audio: false,
+        video: {width: 1280, height: 720}
+      });
+      /* use the stream */
+      divVideoChatLobby.style = 'display:none'
+      userVideo.srcObject = stream;
+      userVideo.onloadedmetadata = function(e) {
+        userVideo.play();
+      };
+      
+      roomInput.value === ''
+    } catch(err) {
+      /* handle the error */
+      console.log(err);
+      alert('could not get the user device')
+    }
+  }
+  getMedia()
+})
+
+socket.on('joined', function() {
+  let creator = false
+
+  async function getMedia(constraints) {
+    let stream = null;
       try {
         stream = await navigator.mediaDevices.getUserMedia({
           audio: false,
@@ -35,5 +70,10 @@ joinButton.addEventListener('click', function(){
       }
     }
     getMedia()
-  }
 })
+socket.on('full', function() {
+  alert('room is full, can not join')
+})
+socket.on('ready', function() {})
+socket.on('offer', function() {})
+socket.on('answer', function() {})
